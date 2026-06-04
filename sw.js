9ab1a1fc95e12240f28377ad56f63d16605d603e
Nery <renannery@gmail.com>
2026-06-03 22:04:17 -0500

    Corrige sequencia/acordes de 'A Batucada dos Nossos Tantas' (Intro/Verso/Refrao, Tom F)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // SambaKY Songbook - service worker (offline cache)
 // manter em sincronia com APP_VERSION no index.html
-const CACHE = 'sambaky-songbook-2026.06.03-2157';
+const CACHE = 'sambaky-songbook-2026.06.03-2204';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './logo-src.svg',
                 './icon-180.png', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 
